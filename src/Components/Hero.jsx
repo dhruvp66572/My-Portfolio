59bf3edffd85e044b72fdcb7cea2dcdc784bf7b9
DHRUV PRAJAPATI <dhruvprajapati66572@gmail.com>
2024-06-02 16:37:45 +0530

    first commit

---
+++ src/Components/Hero.jsx
@@ -0,0 +1,69 @@
+import React from "react";
+import { HERO_CONTENT } from "../constants/index";
+import profilePic from "../assets/kevinRushProfile.png";
+import { motion } from "framer-motion";
+
+const containerVariants = (delay) => {
+  return {
+    hidden: {
+      opacity: 0,
+      x: -100,
+    },
+    visible: {
+      opacity: 1,
+      x: 0,
+      transition: {
+        delay: delay,
+        duration: 0.5,
+      },
+    },
+  };
+};
+
+const Hero = () => {
+  return (
+    <div className="border-b border-neutral-900 pb-4 lg:mb-35">
+      <div className="flex flex-wrap">
+        <div className="w-full lg:w-1/2">
+          <div className="flex flex-col items-center lg:items-start">
+            <motion.h1
+              variants={containerVariants(0)}
+              initial="hidden"
+              animate="visible"
+              className="pb-16 text-6xl font-thin tracking-tight lg:mt-16 lg:text-8xl "
+            >
+              Dhruv Prajapati
+            </motion.h1>
+            <motion.span
+              variants={containerVariants(0.5)}
+              initial="hidden"
+              animate="visible"
+              className="bg-gradient-to-r from-pink-300 via-slate-500 to-purple-500 bg-clip-text text-3xl tracking-tight text-transparent"
+            >
+              Full Stack Developer
+            </motion.span>
+            <motion.p
+              variants={containerVariants(1)}
+              initial="hidden"
+              animate="visible"
+              className="my-2 max-w-xl py-6 font-light tracking-tighter"
+            >
+              {HERO_CONTENT}
+            </motion.p>
+          </div>
+        </div>
+        <div className="w-full lg:w-1/2 lg:p-8">
+          <div className="flex justify-center">
+            <motion.img
+                initial={{ x:100,opacity: 0 }}
+                animate={{x:0,opacity: 1}}
+                transition={{delay: 1.2,duration: 1}}
+            src={profilePic} alt="" />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,38 @@
+import React from "react";
+import { FaGithub, FaInstagram, FaLinkedin } from "react-icons/fa";
+import { FaXTwitter } from "react-icons/fa6";
+
+const Navbar = () => {
+  return (
+    <nav className="mb-20 flex items-center justify-between py-6">
+      <div className="flex flex-shrink-0 items-center">
+        {/* <img className="mx-2 w-10" src={logo} alt="" /> */}
+        <h1 className="text-3xl font-bold font-sans ">DP</h1>
+      </div>
+      <div className="m-8 flex items-center justify-center gap-4 text-2xl">
+        <FaLinkedin
+          className="cursor-pointer hover:text-[#0A66C2] transition-all duration-300 ease-in-out"
+          onClick={() =>
+            window.open(
+              "https://www.linkedin.com/in/dhruv-prajapati-088721260/"
+            )
+          }
+        />
+        <FaGithub
+          className="cursor-pointer hover:text-[#333] transition-all duration-300 ease-in-out"
+          onClick={() => window.open("https://github.com/dhruvp66572")}
+        />
+        <FaXTwitter
+          className="cursor-pointer hover:text-[#1DA1F2] transition-all duration-300 ease-in-out"
+          onClick={() => window.open("https://x.com/Dhruv_P_572")}
+        />
+        <FaInstagram
+          className="cursor-pointer hover:text-[#C13584] transition-all duration-300 ease-in-out"
+          onClick={() => window.open("https://www.instagram.com/dp__572/")}
+        />
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
